c5d36019d6e4b2752226a3cebfeb222dd941c016
cedbosse <bosse.ced14@hotmail.com>
2020-09-23 12:25:05 -0400

    Walking W + probuilder

--- ProjetParcourMortel/Assets/Code/Script/MouvementComponent.cs
+++ ProjetParcourMortel/Assets/Code/Script/MouvementComponent.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class MouvementComponent : MonoBehaviour
 {
+    private Animator animator;
     // Start is called before the first frame update
     void Awake()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+            animator.SetBool("isWalking", true);
     }
 }
