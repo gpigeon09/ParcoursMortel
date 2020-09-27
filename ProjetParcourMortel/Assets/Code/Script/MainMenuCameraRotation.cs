96d6763fbf04bb74cab00825bc01e6d188ac5e2e
Dylann Fontus <58182692+Dylannord@users.noreply.github.com>
2020-09-26 23:08:46 -0400

    Menu Principal Ajouté
    Menu Principal

---
+++ ProjetParcourMortel/Assets/Code/Script/MainMenuCameraRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCameraRotation : MonoBehaviour
+{
+    public float speed = 1;
+    void Update()
+    {
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+    }
+}
